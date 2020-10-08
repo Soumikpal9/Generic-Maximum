045008dcb31187b27542dc21936e9f6c9584f717
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-08 11:26:14 +0530

    [SP]Added: Maximum Test For Integer Added

--- src/main/java/Generic/Maximum/MaximumTest.java
+++ src/main/java/Generic/Maximum/MaximumTest.java
@@ -4,5 +4,14 @@
 package Generic.Maximum;
 
 public class MaximumTest {
-    
+    public static Integer maximum(Integer a, Integer b, Integer c) {
+    	Integer max = a;
+    	if(b.compareTo(max) > 0) {
+    		max = b;
+    	}
+    	if(c.compareTo(max) > 0) {
+    		max = c;
+    	}
+    	return max;
+    }
 }
